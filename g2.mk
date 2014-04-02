c7d8eb616653def957e8a0e7f6fbc2efc48bd1d2
houst0nn <houstonn@gmail.com>
2014-04-02 06:59:13 -0400

    g2: squash a few commits from CM
    ril: Ignore unsupported IMS state request
    These requests are left dangling, return an explicit not-supported
    reply to avoid timeouts
    Change-Id: I5ddecf43e112c64ea719c5b753993b5b8f0d5dde
    
    Use input switches to detect audio jack insertions
    Change-Id: Icd845f5dfa043515e312b5b66df379e00297ed4b
    
    enable loki property
    support fuse various fstypes for usb storage (exfat, ntfs...)
    Change-Id: I0555bbac73fb98c20570748d17558ebd35573b5d
    
    credits: rmcc, PhilZ-cwm6
    
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- g2.mk
+++ g2.mk
@@ -105,7 +105,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.sf.lcd_density=480 \
-	ro.opengles.version=196608
+	ro.opengles.version=196608 \
+	ro.loki_enabled=1
 
 # Audio Configuration
 PRODUCT_PROPERTY_OVERRIDES += \
